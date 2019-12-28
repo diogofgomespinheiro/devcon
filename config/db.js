476f1d0b3511d8ca22aa73c659eb560cbece3ab7
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-12-28 20:19:28 +0000

    created endpoint to create or update user profile

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
+      useFindAndModify: false
     });
 
     console.log("MongoDB Connected...");
