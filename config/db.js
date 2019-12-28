92f958e27463325262b8406e2aa2ba61a0cfba9a
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-12-28 17:54:54 +0000

    created register user endpoint

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true
     });
 
     console.log("MongoDB Connected...");
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
