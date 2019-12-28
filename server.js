b2fadcbc3dbd8b458212fb18cd72f77db8643aca
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-12-28 17:19:28 +0000

    added routes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+if (process.env.NODE_ENV !== "production") dotenv.config();
 
 const app = express();
 
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
