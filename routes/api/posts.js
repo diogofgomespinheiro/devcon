031cf8e3f9c8a174640141dc619f0bba56fe877d
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-12-29 13:15:31 +0000

    created endpoint to add posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check } = require("express-validator");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => {
-  res.send("Posts route");
-});
+const PostsController = require("../../controllers/posts");
 
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").notEmpty()]],
+  PostsController.createPost
+);
 
 module.exports = router;
