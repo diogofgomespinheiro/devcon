214f392fba74a1f2007de9264874b3cd4aecbf99
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-12-30 01:44:25 +0000

    created register page

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,14 +1,93 @@
 //Library imports
-import React from "react";
+import React, { useState } from "react";
 
 //Style imports
 import "./styles.css";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const { name, email, password, confirmPassword } = formData;
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      console.log("Passwords do not match")
+    } else {
+      console.log(formData);
+    }
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            minLength="6"
+            value={confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </>
   );
 };
 
