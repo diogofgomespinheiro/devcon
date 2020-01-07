60920ddbf3d43a4ebea7d6c6cd128697a64f3797
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-07 02:30:26 +0000

    build: prepared app for heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
+const compression = require("compression"); 
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") dotenv.config();
 
@@ -20,6 +23,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"client/build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
